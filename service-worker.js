6fcf51de411264d62111a65173c98e79e011bac7
Rahul Jangu <rahuljangu621@gmail.com>
2026-03-11 12:49:41 +0530

    Update service worker caching strategy and files

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,35 @@
-const CACHE_NAME = "travel-site-v1";
+const CACHE_NAME = "travel-cache-v1";
 
 const urlsToCache = [
-  '/Travel/',
-  "/Travel/index.html",
-  "/Travel/style.css",
-  "/Travel/script.js"
+"./",
+"./index.html",
+"./style.css",
+"./script.js"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching files");
-      return cache.addAll(urlsToCache);
-    })
-  );
+// INSTALL EVENT
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME).then(cache => {
+console.log("Files Cached");
+return cache.addAll(urlsToCache);
+})
+
+);
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+// FETCH EVENT
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+
+);
+
 });
